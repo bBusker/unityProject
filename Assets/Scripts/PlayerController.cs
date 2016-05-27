a7f63b3cdfa1bd88549731b335f548bc767d1015
Peaceserenity <mookins@live.ca>
2016-05-27 02:24:42 -0600

    Pick up!
    -Snake pick up with the object being picked up destroyed and a new pick
    up object spawning.
    -Score system
    -TODO: Create better random AI (make sure that food doesn't spawn too
    close to the same spot as before)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,25 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     public float angaccel;
+    public int foodCount;
+    public int scoreCount;
+    public Text score;
     public GameObject clone;
+    public Collider2D food;
 
     private Rigidbody2D rb2d;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        scoreCount = 0;
+        foodCount = 0;
+        SetCountText();
+        while (foodCount == 0)
+        {
+            food.gameObject.SetActive(true);
+            foodCount += 1;
+        }
     }
 
     void FixedUpdate()
@@ -25,19 +37,27 @@ public class PlayerController : MonoBehaviour {
         rb2d.AddTorque(torque);
     }
 
-    /*void Update()
-    {
-        transform.Rotate(new Vector3(0, 0, 70) * Time.deltaTime);
-    }*/
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D food)
     {
-        if (other.gameObject.CompareTag("Pickup"))
+        if (food.gameObject.CompareTag("Pickup"))
         {
-            other.gameObject.SetActive(false);
-            Instantiate(clone);//, transform.position, Quaternion.identity);
+            Instantiate(food, new Vector2(Random.Range(-10.0F, 10.0F), Random.Range(-10.0F, 10.0F)), Quaternion.identity);
+            food.gameObject.SetActive(false);
+            Destroy(food.gameObject);
+            scoreCount += 1;
+            foodCount -= 1;
+            SetCountText();
+
+            /**
+             * Tails need fixing. The more tails there are, the harder it is to control the snake (ie. Snake gets pushed by the tail)
+             */
+            //Instantiate(clone);
         }
     }
 
-
+    void SetCountText()
+    {
+        score.text = "SCORE: " + scoreCount.ToString();
+    }
 }
