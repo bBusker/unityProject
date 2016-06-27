e6e732f57a4895e328e9b8642795c7b93c9e9cf8
AntiSeed <bob.jack.f@gmail.com>
2016-06-26 21:19:49 -0600

    Pause Menu
    Added pause menu and resume buttons. Will pause game on esc, unpause on
    Resume or esc

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseController : MonoBehaviour {
+    public Transform canvas; 
+
+	void Start () {
+	}
+	
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }
+                    
+        }
+	}
+}
